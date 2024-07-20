7197271c1f558a58c74ffc267e274901abd31431
Babacar Thiam <141820773+babacarth7@users.noreply.github.com>
2024-07-20 13:37:15 +0000

    Initial commit

---
+++ src/main/java/com/babacarthiam/springbootwithdockercompose/SpringbootWithDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.babacarthiam.springbootwithdockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithDockerComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithDockerComposeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/babacarthiam/springbootwithdockercompose/SpringbootWithDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babacarthiam.springbootwithdockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithDockerComposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
